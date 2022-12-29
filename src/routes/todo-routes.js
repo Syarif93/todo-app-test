57eba7e6dcc2923b88a2d4d3452041acaa7ef8ad
Syarif93 <moh.syarif93@gmail.com>
2022-12-30 06:41:06 +0800

    add docker-compose file

--- src/routes/todo-routes.js
+++ src/routes/todo-routes.js
@@ -9,12 +9,112 @@ router.get('/todo-items', async (req, res) => {
     const todos = await Todo.findAll()
 
     if(todos.length > 0) {
-      return res.json(todos)
+      return res.status(200).json({
+        status: "Success",
+        message: "Success",
+        data: todos
+      })
     }
 
     return res.status(404).json({
       status: "Empty",
       message: "Todos Is Empty"
+    })
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+router.get('/todo-items/:todo_id', async (req, res) => {
+  const { todo_id } = req.params
+
+  try {
+    const todo = await Todo.findByPk(todo_id)
+
+    if(todo) {
+      return res.status(200).json({
+        status: "Success",
+        message: "Success",
+        data: todo
+      })
+    }
+
+    return res.status(404).json({
+      status: "Not Found",
+      message: "Todo Not Found"
+    })
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+router.post('/todo-items', async (req, res) => {
+  const { title, activity_group_id, is_active } = req.body
+
+  try {
+    const todo = await Todo.create({ title, activity_group_id, is_active })
+
+    if(todo) {
+      return res.status(201).json({
+        status: "Success",
+        message: "Success",
+        data: todo
+      })
+    }
+
+    return res.status(404).json({
+      status: "Failed",
+      message: "Create Todo Failed"
+    })
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+router.patch('/todo-items/:todo_id', async (req, res) => {
+  const { todo_id } = req.params
+  const { title, priority, is_active } = req.body
+
+  try {
+    const todo = await Todo.findByPk(todo_id)
+
+    if(todo) {
+      await todo.update({ title, priority, is_active })
+
+      return res.status(200).json({
+        status: "Success",
+        message: "Success",
+        data: todo
+      })
+    }
+
+    return res.status(404).json({
+      status: "Not Found",
+      message: "Todo Not Found"
+    })
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
+router.delete('/todo-items/:todo_id', async (req, res) => {
+  const { todo_id } = req.params
+
+  try {
+    const todo = await Todo.findByPk(todo_id)
+
+    if(todo) {
+      await todo.destroy()
+
+      return res.status(200).json({
+        status: "Success",
+        message: "Success"
+      })
+    }
+
+    return res.status(404).json({
+      status: "Not Found",
+      message: "Todo Not Found"
     })
   } catch (error) {
     return res.status(500).json(error)
